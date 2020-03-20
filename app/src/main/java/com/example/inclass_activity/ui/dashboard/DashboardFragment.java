b8a0271133bbc7aae1537a90ca04fb173fe76611
gowthamkgv <gowtham.varma.kanumuru@smu.ca>
2020-03-20 16:43:46 -0300

    Submit

---
+++ app/src/main/java/com/example/inclass_activity/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,37 @@
+package com.example.inclass_activity.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.inclass_activity.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextInputLayout firstNameLayout = view.findViewById(R.id.first_name_text_input_layout);
+
+    }
+}
